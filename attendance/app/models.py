0f8360ef63e84045527b914304f3a260b3e69ce1
Mohamed Said <m.luckysniper@gmail.com>
2020-12-23 10:09:35 +0200

    adding basic models

--- attendance/app/models.py
+++ attendance/app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):  
+
+    first_name = models.CharField(max_length=50,null=True)
+    last_name = models.CharField(max_length=50,null=True)
+    department = models.ForeignKey( "Department" , on_delete = models.CASCADE , null=True, blank=True)
+    branch = models.ForeignKey( "Branch" , on_delete = models.CASCADE , null=True, blank=True)
+    sort = models.IntegerField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+    joining = models.DateField( null=True)
+    pph = models.FloatField(null=True,blank=True,default=0)
+    pont = models.FloatField(null=True,blank=True,default=0)
+
+    def get_full_name(self):
+        return '%s %s'%(self.first_name,self.last_name)
+
+    def __str__(self):
+        return '%s %s'%(self.first_name,self.last_name)
+
+class Branch(models.Model):
+    title = models.CharField(max_length=50,null=True)
+    sort = models.IntegerField(null=True, blank=True)
+    def __str__(self):
+        return "[%s] %s "%(self.id,self.title)
+
+class Department(models.Model):
+    
+    title = models.CharField(max_length=50,null=True)
+    sort = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return "[%s] %s "%(self.id,self.title)
+
+
+
